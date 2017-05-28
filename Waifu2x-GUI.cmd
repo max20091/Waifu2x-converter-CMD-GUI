676240018a752fae12d61dee121d2d99b4a2e5bc
Phuc Dat Bich <boostyourprogram@gmail.com>
2017-05-28 15:25:47 +0700

    Update Waifu2x-GUI.cmd
    Added auto shutdown function
    Redesigned code structures, no more spaghetti codes
    Better looking, reduced file size

--- Waifu2x-GUI.cmd
+++ Waifu2x-GUI.cmd
@@ -1,43 +1,48 @@
 @echo off
 :config
-set ScaleFactor=2
-Set ModelDir=models_rgb\
-set NoiseLevel=0
+Rem You can edit config here
 set w2xcexe=waifu2x-converter-cpp.exe
+set ScaleFactor=2
+set NoiseLevel=0
+Set ModelDir=models_rgb\
 set BlockSize=512
+set AutoShutDown=0
 
 :MainMenu
 cls
 title Waifu2x simple GUI by max20091
 If not exist %w2xcexe% echo waifu2x converter not found! Please check again if you put this file at the right place.
 echo scale factor: %ScaleFactor%x
+if %NoiseLevel% equ 0 (echo Disabled image denoising) else (echo Noise level: %NoiseLevel%)
 echo Model directory: %ModelDir%
-echo Noise level: %NoiseLevel%
 echo Block size: %BlockSize%
-cmdmenusel f8%f0 "Run" "Change scale factor" "Change noise level" "Change Model Directory" "Change block size" "Convert to PNG" "Exit"
+if %AutoShutDown% equ 1 (echo Auto shutdown: on) else (echo Auto shutdown: off)
+echo ------------------------
+cmdmenusel f8%f0 "Run upscale" "Convert to PNG" "Change scale factor" "Change noise level" "Change Model Directory" "Change block size" "Auto shutdown toggle on/off" "Exit"
 if %ERRORLEVEL% == 1 goto Scale
-if %ERRORLEVEL% == 2 goto ChangeScaleFactor
-if %ERRORLEVEL% == 3 goto ChangeNoiseLevel
-if %ERRORLEVEL% == 4 goto ChangeModelDir
-if %ERRORLEVEL% == 5 goto ChangeBlockSize
-if %ERRORLEVEL% == 6 goto ConvertToPNG
-if %ERRORLEVEL% == 7 exit
-:MainMenuCompleted
-title Waifu2x simple GUI by max20091
+if %ERRORLEVEL% == 2 goto ConvertToPNG
+if %ERRORLEVEL% == 3 goto ChangeScaleFactor
+if %ERRORLEVEL% == 4 goto ChangeNoiseLevel
+if %ERRORLEVEL% == 5 goto ChangeModelDir
+if %ERRORLEVEL% == 6 goto ChangeBlockSize
+if %ERRORLEVEL% == 7 if %AutoShutDown% equ 0 (set AutoShutDown=1) else (set AutoShutDown=0)
+if %ERRORLEVEL% == 8 exit
+goto MainMenu
+
+:scale
+title Upscaling...
 cls
-echo Completed!
-echo scale factor: %ScaleFactor%x
-echo Model directory: %ModelDir%
-echo Noise level: %NoiseLevel%
-echo Block size: %BlockSize%
-cmdmenusel f8%f0 "Run" "Change scale factor" "Change noise level" "Change Model Directory" "Change block size" "Convert to PNG" "Exit"
-if %ERRORLEVEL% == 1 goto Scale
-if %ERRORLEVEL% == 2 goto ChangeScaleFactor
-if %ERRORLEVEL% == 3 goto ChangeNoiseLevel
-if %ERRORLEVEL% == 4 goto ChangeModelDir
-if %ERRORLEVEL% == 5 goto ChangeBlockSize
-if %ERRORLEVEL% == 6 goto ConvertToPNG
-if %ERRORLEVEL% == 7 exit
+if %NoiseLevel% equ 0 (FOR /f "delims=" %%a IN ('dir /b input\*.*') DO call %w2xcexe% --scale_ratio %ScaleFactor% --model_dir %ModelDir% -m scale --block_size %BlockSize% -i "input\%%a" -o "output\%%~na.png") else (FOR /f "delims=" %%a IN ('dir /b input\*.*') DO call %w2xcexe% --scale_ratio %ScaleFactor% --model_dir %ModelDir% -m noise_scale --noise_level %NoiseLevel% --block_size %BlockSize% -i "input\%%a" -o "output\%%~na.png")
+if %AutoShutDown% equ 1 shutdown -s
+pause
+goto MainMenu
+
+:ConvertToPNG
+cls
+FOR /f "delims=" %%a IN ('dir /b input\*.*') DO call %w2xcexe% --scale_ratio 1 --model_dir %ModelDir% -m scale -i "input\%%a" -o "output\%%~na.png"
+if %AutoShutDown% equ 1 shutdown -s
+pause
+goto MainMenu
 
 :ChangeScaleFactor
 title Change scale factor
@@ -61,6 +66,12 @@ if %ERRORLEVEL% == 4 set NoiseLevel=3
 if %ERRORLEVEL% == 5 goto MainMenu
 goto MainMenu
 
+:ChangeModelDir
+title Change model directory
+cls
+set /p ModelDir=Put your model directory name to here (Default is models_rgb\): 
+goto MainMenu
+
 :ChangeBlockSize
 title Change block size
 cls
@@ -72,21 +83,3 @@ if %ERRORLEVEL% == 3 set BlockSize=512
 if %ERRORLEVEL% == 4 set /p BlockSize= Put your block size here: 
 if %ERRORLEVEL% == 5 goto MainMenu
 goto MainMenu
-
-:ConvertToPNG
-cls
-FOR /f "delims=" %%a IN ('dir /b input\*.*') DO call %w2xcexe% --scale_ratio 1 --model_dir %ModelDir% -m scale -i "input\%%a" -o "output\%%~na.png"
-goto MainMenuCompleted
-
-:ChangeModelDir
-title Change model directory
-cls
-set /p ModelDir=Put your model directory name to here (Default is models_rgb\): 
-goto MainMenu
-
-:scale
-title Upscaling...
-cls
-if %NoiseLevel% equ 0 (FOR /f "delims=" %%a IN ('dir /b input\*.*') DO call %w2xcexe% --scale_ratio %ScaleFactor% --model_dir %ModelDir% -m scale --block_size %BlockSize% -i "input\%%a" -o "output\%%~na.png") else (FOR /f "delims=" %%a IN ('dir /b input\*.*') DO call %w2xcexe% --scale_ratio %ScaleFactor% --model_dir %ModelDir% -m noise_scale --noise_level %NoiseLevel% --block_size %BlockSize% -i "input\%%a" -o "output\%%~na.png")
-pause
-goto MainMenuCompleted
